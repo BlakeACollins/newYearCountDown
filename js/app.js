5d31e38f1c45be74e47ceec22378756228ce9c7a
Blake Collins <blakeac369@gmail.com>
2020-08-06 16:15:07 -0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,40 @@
+const days = document.getElementById('days'),
+    hours = document.getElementById('hours'),
+    minutes = document.getElementById('minutes'),
+    seconds = document.getElementById('seconds'),
+    countdown = document.getElementById('countdown'),
+    year = document.getElementById('year'),
+    loading = document.getElementById('loading');
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+
+// Set background year
+year.innerText = currentYear + 1;
+
+// Update countdown timer
+function updateCountdown() {
+  const currentTime = new Date();
+  const diff = newYearTime - currentTime;
+
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  const s = Math.floor(diff / 1000) % 60;
+
+  // Add values to DOM
+  days.innerHTML = d;
+  hours.innerHTML = h < 10 ? '0' + h : h;
+  minutes.innerHTML = m < 10 ? '0' + m : m;
+  seconds.innerHTML = s < 10 ? '0' + s : s;
+}
+
+//Show spinner on reload
+setTimeout(() =>{
+    loading.remove();
+    countdown.style.display = 'flex';
+}, 1000)
+
+// Run every second
+setInterval(updateCountdown, 1000);
